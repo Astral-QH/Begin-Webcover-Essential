ff978869dd525962ea783976f7ba61a48c84233b
Quang Huy <143514167+Astral-QH@users.noreply.github.com>
2023-11-18 13:13:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    $('.lasted-project-carousel').owlCarousel({
+        loop: true,
+        margin: 20, 
+        dots: true,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 4
+            },
+            1200: {
+                items: 4
+            }
+        }
+    })
+
+    $('.testimonials-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+
